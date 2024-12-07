811e6af05900f0274cc35280550c63a2c7af50d7
yesus011 <jesu8907@gmail.com>
2024-12-07 00:28:51 +0000

    1.0Edition

--- src/models.py
+++ src/models.py
@@ -7,23 +7,45 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base): 
+    __tablename__ = 'user' 
+    id = Column(Integer, primary_key=True) 
+    username = Column(String, unique=True, nullable=False) 
+    email = Column(String, unique=True, nullable=False) 
+    password = Column(String, nullable=False) 
+    firstname = Column(String) lastname = Column(String) 
+     
+    favorites = relationship('Favorite', back_populates='user')
+    
+class Planet(Base):
+     __tablename__ = 'planet' 
+     id = Column(Integer, primary_key=True) 
+     name = Column(String, unique=True, nullable=False) 
+     climate = Column(String) 
+     terrain = Column(String) 
+     population = Column(Integer) 
+     favorites = relationship('Favorite', back_populates='planet') 
+     
+class Character(Base): 
+    __tablename__ = 'character' 
+    id = Column(Integer, primary_key=True) 
+    name = Column(String, unique=True, nullable=False) 
+    species = Column(String) 
+    homeworld = Column(String) 
+    affiliation = Column(String) 
+    favorites = relationship('Favorite', back_populates='character') 
+     
+class Favorite(Base):
+     __tablename__ = 'favorite' 
+     id = Column(Integer, primary_key=True) 
+     user_id = Column(Integer, ForeignKey('user.id'), nullable=False) 
+     planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True) 
+     character_id = Column(Integer, ForeignKey('character.id'), nullable=True) 
+     user = relationship('User', back_populates='favorites') 
+     planet = relationship('Planet', back_populates='favorites') 
+     character = relationship('Character', back_populates='favorites')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
 
     def to_dict(self):
         return {}
